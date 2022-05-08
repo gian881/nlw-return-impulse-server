8e20eb5e5759cbf1f04eff4f30af3e0f4aaabc11
Gian <gdass8000@gmail.com>
2022-05-08 19:43:08 -0300

    Try catch routes

--- src/routes.ts
+++ src/routes.ts
@@ -5,19 +5,20 @@ import { SubmitFeedbackUseCase } from './use-cases/submit-feedback-use-case';
 
 export const routes = express.Router();
 
-
-
-
 routes.post('/feedbacks', async (req, res) => {
     const { type, comment, screenshot } = req.body;
-
-    const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
-    const nodemailerMailAdapter = new NodemailerMailAdapter()
+    try{
+        const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+        const nodemailerMailAdapter = new NodemailerMailAdapter()
+        
+        const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository, nodemailerMailAdapter);
     
-    const submitFeedbackUseCase = new SubmitFeedbackUseCase(prismaFeedbacksRepository, nodemailerMailAdapter);
+        await submitFeedbackUseCase.execute({ type, comment, screenshot });
+        return res.status(201).send();
+    } catch (error) {
+        console.error(error);
 
-    await submitFeedbackUseCase.execute({ type, comment, screenshot });
+        return res.status(500).send(error);
+    }
 
-
-    return res.status(201).send();
 });
